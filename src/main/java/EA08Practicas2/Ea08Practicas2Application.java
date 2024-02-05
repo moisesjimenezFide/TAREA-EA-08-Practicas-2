42a52490531fec41d58f67b4361465cb17100527
moise <moise@DESKTOP-AD1QOT1>
2024-02-04 19:28:31 -0600

    Tarea Semana 3 EA-08-Practicas-2

---
+++ src/main/java/EA08Practicas2/Ea08Practicas2Application.java
@@ -0,0 +1,13 @@
+package EA08Practicas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ea08Practicas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ea08Practicas2Application.class, args);
+	}
+
+}
---
+++ src/test/java/EA08Practicas2/Ea08Practicas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package EA08Practicas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ea08Practicas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
